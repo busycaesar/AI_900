e471b420e612ff979bda00762f24b17e19f33910
Dev J. Shah <dev-jshah@outlook.com>
2025-04-26 18:07:56 -0400

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
 
   themeConfig: {
     navbar: {
-      title: "AI 900",
+      title: "AI 900 Notes",
       logo: {
         alt: "ShahTech Logo",
         src: "img/logo.svg",
